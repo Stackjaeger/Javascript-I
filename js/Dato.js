2de149d0744e3b990e44b4a9e7b5d4d91a655c35
LEL <43479365+Stackjaeger@users.noreply.github.com>
2023-07-20 16:36:00 -0700

    Update dato.js

---
+++ js/Dato.js
@@ -0,0 +1,23 @@
+class Dato {
+  constructor(descripcion, valor) {
+    // Aquí puedes inicializar las propiedades de la clase padre
+    this._descripcion = descripcion;
+    this._valor = valor;
+  }
+  // Aquí puedes agregar los métodos que necesites para manejar los datos
+  get descripcion() {
+    return this._descripcion;
+  }
+
+  set descripcion(descripcion) {
+    this._descripcion = descripcion;
+  }
+
+  get valor() {
+    return this._valor;
+  }
+
+  set valor(valor) {
+    this.valor = valor;
+  }
+}
